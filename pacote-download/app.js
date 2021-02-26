4e3259e4f7275e34da4aa9ce9571710e9a6fcf92
lucasvinicius667 <aokiemailpro@gmail.com>
2021-02-26 10:17:32 -0300

    commit full course

---
+++ pacote-download/app.js
@@ -0,0 +1,29 @@
+const app = require('express')();
+const consign = require('consign');
+const knex = require('knex');
+const knexfile = require('../knexfile');
+//const knexLogger = require('knex-logger');
+
+//Criar chaveamento dinâmico
+app.db = knex(knexfile.test);
+
+//app.use(knexLogger(app.db));
+
+consign({ cwd: 'src', verbose: false})
+    .include('./config/middleware.js')  
+    .then('./services')
+    .then('./routes')
+    .then('./config/routes.js')
+    .into(app);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+
+app.get('/', (req, res) => {
+    res.status(200).send();
+});
+/*
+app.db.on('query', (query) => {
+    console.log({sql: query.sql, bindings: query.bindings ? query.bindings.join(',') : ''});
+})
+    .on('query-response', response => console.log(response))
+    .on('error', error => console.log(error));
+*/
+module.exports = app;
